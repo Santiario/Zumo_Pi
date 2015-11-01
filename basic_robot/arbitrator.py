810a49b689bc63b7e3b235c1bad4937e0fd1afd8
Vemund Santi <vemund@santi.no>
2015-11-01 15:47:23 +0100

    Made bestaa_oving.py. It initializes the robot

--- basic_robot/arbitrator.py
+++ basic_robot/arbitrator.py
@@ -1,20 +1,19 @@
-# import modules here
 from operator import attrgetter
 
 __author__ = 'estensen'
 
 
 class Arbitrator:
-    def __init__(self, bbcon):
-        self.bbcon = bbcon
+    def __init__(self):
+        pass
 
-    def choose_action(self):
+    def choose_action(self, active_behaviors):
         """Choose winning behavior.
 
         Returns
         -------
         motor_recommendation : On the format (action, duration).
         """
-        winning_behavior = max(self.bbcon.active_behaviors, key=attrgetter('weight'))
+        winning_behavior = max(active_behaviors, key=attrgetter('weight'))
         print("Winning behavior is " + str(winning_behavior))
         return winning_behavior.motor_recommendation
--- basic_robot/bestaa_oving.py
+++ basic_robot/bestaa_oving.py
@@ -1,18 +1,27 @@
 from sensors import *
 from sensobs import *
 from behaviors import *
+from arbitrator import Arbitrator
+from motob import Motob
+from BBCON import Bbcon
 
 
 def main():
     ultrasonic_sensor = Ultrasonic()
-    #reflectance_sensor = ReflectanceSensors()
-    #camera_sensor = Camera()
+    # reflectance_sensor = ReflectanceSensors()
+    # camera_sensor = Camera()
 
     ultrasonic_sensob = Sensob(ultrasonic_sensor)
 
     ultrasonic_behavior = SonicBehavior(ultrasonic_sensob)
 
-    
+    arbitrator = Arbitrator()
+
+    motob = Motob()
+
+    bbcon = Bbcon([ultrasonic_behavior], [ultrasonic_behavior], [ultrasonic_sensob], motob,arbitrator)
+
+    bbcon.run_one_timestep()
 
 
 
