5c4b9b688af10da5c18740f3d28ca3fe86531f66
Jostein Brevik <jostein.brevik@gmail.com>
2015-11-02 12:49:47 +0100

    Start reflectancesensob

--- basic_robot/sensobs.py
+++ basic_robot/sensobs.py
@@ -29,17 +29,16 @@ from imager2 import Imager
 class CameraSensob(Sensob):
 
 
-    def __init__(self, sensor, color_treshold=1000):
-        """Initialize bbcon.
+    def __init__(self, sensor, color_treshold=1700):
+        """
 
         Parameters
         ----------
-        sensobs: sensors used. Here only camera shoult be added
-        active_flag: whether to use this behavior or not
-        priority: how important this behavior is (very important)
+        sensors: sensor used. Here it's a camera
         color: which color to look for. 0 = red, 1 = green, 2 = blue
         color_treshhold: amount of a colour needed in order to register as that color.
         """
+        self.color = 0
         self.sensor = sensor
         self.value = self.sensor.update()
         self.color_treshold = color_treshold
@@ -50,18 +49,34 @@ class CameraSensob(Sensob):
         If there's enough red, motor"""
         self.sensor.update()
         taken_image = self.sensor.get_value()
-        wta_image = Imager(image=taken_image).map_color_wta() #colors each pixel the dominant color
+        wta_image = Imager(image=taken_image).map_color_wta(thresh = 0.5) #colors each pixel the dominant color
         red_count = 0
         for i in range(20, 100):
             for j in range(20, 80):
-                if(wta_image.get_pixel(0,0)[0] > 100):
+                if(wta_image.get_pixel(0,0)[color] > 100):
                     red_count += 1
         if(red_count > self.color_treshold):
             self.value = 1.0
         else:
             self.value = 0.0
 
+class ReflectanceSensob(Sensob):
 
+    def __init__(self, sensor):
+        """
+        Parameters
+        ----------
+        sensobs: sensors used. Here only camera shoult be added
+        active_flag: whether to use this behavior or not
+        priority: how important this behavior is (very important)
+        color: which color to look for. 0 = red, 1 = green, 2 = blue
+        color_treshhold: amount of a colour needed in order to register as that color.
+        """
+        self.sensor = sensor
+        self.value = self.sensor.update()
+
+    def update(self):
+        sensors = self.sensor.get_value()
 
 
 
